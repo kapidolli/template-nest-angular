83dc42fae1675f5fc689d4921dbe0512069e4311
kapidolli <jasin.shala.jsh@gmail.com>
2022-01-27 23:42:30 +0100

    project init

---
+++ server/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { join } from 'path';
+import { AppServerModule } from '../src/main.server';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    AngularUniversalModule.forRoot({
+      bootstrap: AppServerModule,
+      viewsPath: join(process.cwd(), 'dist/fullstack-sample-project/browser')
+    }),
+    AuthModule
+  ]
+})
+export class AppModule {}
